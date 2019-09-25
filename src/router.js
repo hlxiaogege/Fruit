598f7d58bede79db89912ffa05f711223660658a
hulexiaogege <hlxiaogege>
2019-09-25 21:22:23 +0800

    vuex

--- src/router.js
+++ src/router.js
@@ -6,7 +6,6 @@ import Carts from './views/Carts'
 import Classify from './views/Classify'
 import Mine from './views/Mine'
 import Search from './views/Search'
-import Mine_setting from './views/Mine_setting'
 
 Vue.use(Router)
 
@@ -17,9 +16,8 @@ export default new Router({
     {path: '/home',component: Home},
     {path: '/carts',component: Carts},
     {path: '/classify',component: Classify},
-    {path: '/mine',name:'mine',component: Mine},
+    {path: '/mine',component: Mine},
     {path: '/search',component: Search},
-    {path: '/mine-setting',name:'mine-setting',component: Mine_setting},
     
   ]
 })
